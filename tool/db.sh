6545eea70d9908da06307e29c4825afd9ee52f18
zbryikt <tkirby@gmail.com>
2025-10-25 18:35:10 +0800

    regen

--- tool/db.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -d -p 15432:5432 -e POSTGRES_USER=pg -e POSTGRES_PASSWORD=pg -e POSTGRES_DB=pg -it datahub-db:latest
--- tool/docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd ..
-docker build -t datahub-backend:latest .
-cd tool/postgres
-docker build -t datahub-db:latest .
-cd ..
